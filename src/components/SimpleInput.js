a13b576db54800ce98225d951fbfb5d975493b95
Chijioke Chika <8pluscreative@gmail.com>
2022-06-03 00:22:42 -0400

    Updating

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,16 +1,11 @@
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [inputName, setInputName] = useState("");
-  const [inputNameIsValid, setInputNameIsValid] = useState(false)
   const [inputNameTouched, setInputNameTouched] = useState(false)
 
-  useEffect(() => {
-    if(inputNameIsValid) {
-      console.log('Opor');
-    }
-  }, [inputNameIsValid])
+  const inputNameIsValid = inputName.trim() !== '';
+  const nameInputIsInvalid = !inputNameIsValid && inputNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setInputName(event.target.value);
@@ -18,11 +13,6 @@ const SimpleInput = (props) => {
 
   const nameInputBlurHandler = event => {
     setInputNameTouched(true)
-
-    if (inputName.trim() === '') {
-      setInputNameIsValid(false)
-      return;
-    }
   };
 
   const formSubmitHandler = (event) => {
@@ -30,20 +20,16 @@ const SimpleInput = (props) => {
 
     setInputNameTouched(true)
 
-    if (inputName.trim() === '') {
-      setInputNameIsValid(false)
+    if (inputNameIsValid) {
       return;
     }
 
     setInputNameIsValid(true)
 
     console.log(inputName);
-    const enteredVal = nameInputRef.current.value;
-    console.log(enteredVal);
+
     setInputName("");
   };
-
-  const nameInputIsInvalid = !inputNameIsValid && inputNameTouched;
 
   const nameInputStyles = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
   
@@ -52,7 +38,6 @@ const SimpleInput = (props) => {
       <div className={nameInputStyles}>
         <label htmlFor="name">Your Name *</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
